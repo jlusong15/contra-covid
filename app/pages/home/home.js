3379eff5ca9da8f2aac9f45317838200108ace99
Jennifer Lusong <jenniferlusong@gmail.com>
2020-11-08 19:27:30 +0800

    Updated homepage

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -14,7 +14,11 @@ angular
   .controller('HomeCtrl', function ($scope, $location) {
     $scope.pageClass = "home";
     $scope.bannerHeader = "Be part of the SOLUTION";
-    $scope.bannerDetails = "ConTra means Contact Tracing. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a augue neque. Integer ullamcorper tempor tempor. Aenean blandit interdum quam, nec pellentesque neque rutrum at. Mauris a risus elit. Vivamus nisl felis."
+    $scope.bannerDetails = [
+      "ConTra means Contact Tracing.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a augue neque. Integer ullamcorper tempor tempor. Aenean blandit.",
+      "To view a sample form using AngularJS, click the button."
+    ];
     
     $scope.contactTracing = function () {
       $location.path('/contact-tracing')
